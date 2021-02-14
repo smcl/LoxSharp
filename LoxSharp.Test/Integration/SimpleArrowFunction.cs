c76082f1a4d6af1c70a9702b9a75ae4550716559
Sean McLemon <sean.mclemon@gmail.com>
2021-02-14 19:23:12 +0100

    basic for/while tests

---
+++ LoxSharp.Test/Integration/SimpleArrowFunction.cs
@@ -0,0 +1,12 @@
+﻿namespace LoxSharp.Test.Integration
+{
+    public class SimpleArrowFunction : LoxTestBase
+    {
+        protected override string Source => @"
+var greet = () => { print ""hello world"" };
+greet();
+";
+
+        protected override string[] ExpectedStdOutLines => new[] { "hello world" };
+    }
+}
---
+++ LoxSharp.Test/Integration/SimpleWhile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoxSharp.Test.Integration
+{
+    public class SimpleWhile : LoxTestBase
+    {
+        protected override string Source => @"
+
+var i = 5;
+while (i > 0) {
+    print i;
+    i = i - 1;
+}
+";
+
+        protected override string[] ExpectedStdOutLines => new[] { "5", "4", "3", "2", "1" };
+    }
+
+    public class SimpleFor : LoxTestBase
+    {
+        protected override string Source => @"
+for (var i = 5; i > 0; i = i - 1) {
+    print i;
+}
+";
+
+        protected override string[] ExpectedStdOutLines => new[] { "5", "4", "3", "2", "1" };
+    }
+}
